4561dc4a4284518a5e066a2ee8912fc4ba9616be
martin1484 <33292152+martin1484@users.noreply.github.com>
2026-02-23 15:24:02 -0300

    Clean code

--- src/App.js
+++ src/App.js
@@ -342,7 +342,7 @@ function App() {
             />
           ))}
         </ul>
-      )};
+      )}
 
       {items.some(item => item.completed) && (
         <button onClick={clearCompleted} className="clear-btn">
